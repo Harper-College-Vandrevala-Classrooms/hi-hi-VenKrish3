d41b8bd4544ffb7bae221d08ac86a465ed906a6f
VenKrish3 <kv72810@mail.harpercollege.edu>
2024-08-21 14:50:54 -0500

    Added on multiple lines

--- hi-hi.cpp
+++ hi-hi.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
+
+int strcmp_case_insensitive(string, string);
 
 int main()
 {
@@ -12,10 +15,41 @@ int main()
 	cout << "Please input the second string" << endl;
 	cin >> str2;
 	cout << endl;
-	return strcmp_case_insensitive(str1,str2);
+	cout << strcmp_case_insensitive(str1,str2);
 }
-
-/*int strcmp_case_insensitive( string s1, string s2)
+	
+int strcmp_case_insensitive(string s1, string s2)
 {
-    
-};*/
+	int final = 0;
+	if(s1.length() < s2.length())
+	{
+		return -1;
+	}
+	else if(s1.length() > s2.length())
+	{
+		return 1;
+	}
+	else{
+		for(int i = 0; i < s1.length(); i++)
+		{
+			if(tolower(s1[i]) > tolower(s2[i]))
+			{
+				return 1;
+				//final++;
+			}
+			else if(tolower(s1[i]) < tolower(s2[i]))
+			{
+				return -1;
+				//final --;
+			}
+		}
+		return 0;
+	}
+	
+	/*if(final > 0)
+		return 1;
+	else if(final < 0)
+		return -1;
+	else 
+		return 0;*/
+};
